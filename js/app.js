0c6951d86545deb894492dc4328b86d5f6914618
kawasaki.taiga <kawasaki.taiga@raccoon.ne.jp>
2019-06-19 10:58:44 +0900

    todoを追加できるように

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@
 	new Vue({
 		el: "#app",
 		data: {
-			title: 'Hello, World'
+			title: 'Hello, World',
+			todos: [],
+			newTodo: ""
+		},
+		methods: {
+			addTodo() {
+				if (!this.newTodo) return;
+				this.todos.push({
+					id: (new Date()).getTime(),
+					title: this.newTodo,
+					completed: false
+				});
+				this.newTodo = "";
+			}
 		}
 	});
 
